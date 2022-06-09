33124341ba8b3cf946b28d64ce547275237d43ec
Haseeb Asif <36411814+HaseebAsif@users.noreply.github.com>
2022-06-09 11:31:58 +0500

    Feat: Javascript Refresher video 2

--- index.js
+++ index.js
@@ -58,4 +58,53 @@
 // console.log(index);
 
 const time = new Date().getHours();
-console.log(time);
+let greeting;
+
+// if (time < 10) {
+//   greeting = "Good Morning";
+// } else if (time < 20) {
+//   greeting = "Good Day";
+// } else {
+//   greeting = "Good Evening";
+// }
+
+// console.log(greeting);
+
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+    break;
+  case 0:
+    day = "Sunday";
+    break;
+
+  default:
+    break;
+}
+
+console.log("Today is " + day);
+
+let text = "";
+for (let i = 0; i < 5; i++) {
+  text += "The number is" + i;
+}
+console.log(text);
