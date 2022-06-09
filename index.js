a5ca586560f8b78f87e8c05f87d8a26d12e1edc7
Haseeb Asif <36411814+HaseebAsif@users.noreply.github.com>
2022-06-09 11:19:58 +0500

    Feat: Javascript Refresher video 1

---
+++ index.js
@@ -0,0 +1,61 @@
+// let name = "John";
+// console.log(name);
+
+// document.getElementById("demo").innerHTML = 31 + 8;
+
+// let city = "London";
+// // alert(city);
+
+// let a = 4;
+// let b = 10;
+// console.log(a + b);
+
+// let x = (100 + 70) * 9;
+// console.log(x);
+
+// let item1 = 5;
+// let item2 = 2;
+// let z = item1 % item2;
+// console.log(z);
+
+// let x = 5;
+// x++;
+// let z = x;
+// console.log(z);
+
+// function sum() {
+//   let a = 4;
+//   let b = 10;
+//   console.log(a + b);
+// }
+
+// sum();
+
+// let text = "abcdefghijklmnopqrstuvwxyz";
+// let length = text.length;
+
+// console.log(length);
+
+// let string = "Apple Banana Orange";
+// let part = string.replace(Banana, Mango);
+// console.log(part);
+
+// let text = "Please visit Microsoft and Microsoft";
+// let newtext = text.replace(/Microsoft/g, "Apple");
+// console.log(newtext);
+
+// let name = "Haseeb";
+// let text2 = name.toLowerCase();
+// console.log(text2);
+
+// let text3 = "Hello";
+// let text4 = "World";
+// let text5 = text3.concat(" ", text4);
+// console.log(text5);
+
+// let text7 = "PLease locate any locate";
+// let index = text7.lastIndexOf("locate");
+// console.log(index);
+
+const time = new Date().getHours();
+console.log(time);
