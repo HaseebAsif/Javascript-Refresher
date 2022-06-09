bd01d53922b5b7353b3bf42e8620fba447ed6f13
Haseeb Asif <36411814+HaseebAsif@users.noreply.github.com>
2022-06-09 12:19:28 +0500

    Feat: Javascript Array Methods

--- index.js
+++ index.js
@@ -108,3 +108,83 @@ for (let i = 0; i < 5; i++) {
   text += "The number is" + i;
 }
 console.log(text);
+
+// let arr1 = [
+//   { id: 1, product: "mouse", countInStock: 2 },
+//   { id: 2, product: "keyboard", countInStock: 0 },
+//   { id: 3, product: "monitor", countInStock: 0 },
+//   { id: 1, product: "watch", countInStock: 4 },
+// ];
+
+// let arr2 = arr1.filter((el, index) => el.countInStock > 0);
+// // output:[
+// //  {id:1,product:'mouse',countInStock:2},
+// //  {id:1,product:'watch',countInStock:4}
+// // ]
+// console.log(arr2);
+
+// let arr1 = [
+//   { id: 1, product: "mouse", countInStock: 2 },
+//   { id: 2, product: "keyboard", countInStock: 1 },
+//   { id: 3, product: "monitor", countInStock: 4 },
+//   { id: 4, product: "watch", countInStock: 4 },
+// ];
+
+// let element = arr1.find((item, index) => item.countInStock > 3);
+// console.log(element);
+// // output:{id:3,product:'monitor',countInStock:4}
+
+// let arr1 = ["MONEY", "laptop", "rug"];
+// let test = arr1.includes("MONEY"); //
+// console.log(test);
+
+// order list
+// let arr1 = [
+//   { id: 1, product: "mouse", quantity: 2, price: 40 },
+//   { id: 2, product: "keyboard", quantity: 0, price: 150 },
+//   { id: 3, product: "monitor", quantity: 4, price: 500 },
+//   { id: 1, product: "watch", quantity: 4, price: 1000 },
+// ];
+
+// let arr2 = arr1.map(({ product, quantity, price }) => ({
+//   product,
+//   total: quantity * price,
+// }));
+// console.log(arr2);
+//  // output:[
+//  // {product:'mouse',total:80 },
+//  //  {product:'keyboard',total:300},
+//  //  {product:'monitor',total:2000},
+//  //  {product:'watch',total:4000}
+//  // ]
+
+// let arr1 = ["MONEY", "monitor", "laptop", "mouse", "laptop", "book", "laptop"];
+// let el = arr1.pop(); // output:laptop
+// console.log(el);
+// console.log(arr1);
+// // arr1=['MONEY', 'monitor', 'laptop', 'mouse', 'laptop', 'book']
+
+// let arr1 = ["MONEY", "monitor", "laptop", "mouse", "laptop", "book", "laptop"];
+// let el = arr1.shift(); // output:MONEY
+// console.log(arr1);
+// // arr1=['MONEY', 'monitor', 'laptop', 'mouse', 'laptop', 'book']
+
+// let arr1 = ["MONEY", "monitor", "laptop", "mouse", "laptop", "book", "laptop"];
+// let el = arr1.push("flask", "chair"); // output: 9
+// console.log(arr1);
+// mouse', 'laptop', 'monitor', 'MONEY']// // arr1=['MONEY', 'monitor', 'laptop', 'mouse', 'laptop', 'book','flask','chair']
+
+// let arr1 = ["MONEY", "monitor", "laptop", "mouse", "laptop", "book", "laptop"];
+// let el = arr1.reverse(); // output: ['laptop', 'book', 'laptop', '
+// console.log(arr1);
+//  el === arr1 -> true
+
+// let arr1 = ["MONEY", "monitor", "laptop", "mouse", "laptop", "book", "laptop"];
+// let arr2 = arr1.slice(); // output:['MONEY', 'monitor', 'laptop', 'mouse', 'laptop', 'book', 'laptop']
+// let arr3 = arr1.slice(1, 4);
+// console.log(arr3);
+
+// let arr1 = ["MONEY", "monitor", "laptop", "mouse", "laptop", "book", "laptop"];
+// let test = arr1.unshift("chair", "desk", "CPU"); // output:9
+// // arr1=['chair','desk','MONEY', 'monitor', 'laptop', 'mouse', 'laptop', 'book', 'laptop']
+// console.log(arr1);
